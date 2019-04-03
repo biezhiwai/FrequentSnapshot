3ea52a79a258d9fead7bdadb4dbad0f0e47b8d8a
hp25 <hp25@localhost.localdomain>
2019-04-03 11:36:34 +0800

    update sh

--- cmake-build-debug/run-size.sh
+++ cmake-build-debug/run-size.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for n in 250000 500000 1000000 2000000 
+for size in 250000 500000 1000000 2000000 
 do
-for m in 0 1 2 3 4 5 6 
+for alg in 0 1 2 3 4 5 6 
 do
-    ./run.sh $m $n 4096 256 0.9
+    ./run.sh $alg $size 4096 256 0.8
     done
 done
--- cmake-build-debug/run-uf.sh
+++ cmake-build-debug/run-uf.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-#uf="16 32 64 128 256"
-uf=256
-for j in ${uf} 
+list="16 32 64 128 256"
+for uf in ${list} 
 do
-for i in 0 1 2 3 4 5 6 
+for alg in 0 1 2 3 4 5 6 
 do
-    ./run.sh $i 250000 4096 $j 0.8
+    ./run.sh ${alg} 1000000 4096 $i{uf} 0.8
     echo "==========================================================="
     done
 done
