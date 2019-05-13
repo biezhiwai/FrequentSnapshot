7b2a5852bfbc78110d75774e687475de6536f00e
bombehub <liliangneu@yeah.net>
2019-05-13 10:12:10 +0800

    add result

--- cmake-build-debug/run.sh
+++ cmake-build-debug/run.sh
@@ -4,9 +4,10 @@ mkdir log
 DB_SIZE=$1
 PAGE_SIZE=$2
 UF=$3
+FILE=$4
 for alg in 0 1 2 3 4 5 6
 do
-./app ${alg} ${DB_SIZE}  ${PAGE_SIZE} ${UF} data.txt
+numactl --cpubind=0 --membind=0 ./app ${alg} ${DB_SIZE}  ${PAGE_SIZE} ${UF} ${FILE}
 done
 echo ==========
 ./result.sh  ${DB_SIZE}  ${PAGE_SIZE} ${UF}
