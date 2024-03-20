827b5eb52fc38a9cbbcb51997553fc9a975b25fb
biezhiwai <1240712385@qq.com>
2024-03-20 22:39:36 +0800

    #fix

--- src/include/myfork_lru.h
+++ src/include/myfork_lru.h
@@ -10,51 +10,6 @@
 #include<pthread.h>
 #include "util.h"
 
-// typedef struct node{
-//     integer index;
-//     char* buf;
-//     struct node* next;
-//     struct node* prev;
-// }node;
-
-// typedef struct{
-//     int size;
-//     node* head;
-//     node* tail;
-// }DoubleList;
-
-// typedef struct{
-//     int capacity;
-//     DoubleList* list;
-//     node** map;
-// }LRUCache;
-
-
-// DoubleList* createDoubleList();
-
-// void addLast(DoubleList* list, node* x);
-
-// void removeX(DoubleList* list, node* x);
-
-// node* removeFirst(DoubleList* list);
-
-// int getSize(DoubleList* list);
-
-// LRUCache* createLRUCache(int capacity);
-
-// void makeRecently(LRUCache* cache, integer index);
-
-// void addRecently(LRUCache* cache, integer index, char* buf);
-
-// void deleteKey(LRUCache* cache, integer index);
-
-// node* removeLeastRecently(LRUCache* cache);
-
-// char* get(LRUCache* cache, integer index);
-
-// node* put(LRUCache* cache, integer index, char* buf);
-
-
 typedef struct {
     integer db_size;
     char *db_small_page;
@@ -62,9 +17,6 @@ typedef struct {
     integer small_page_size;
     integer huge_page_size;
     float huge_page_ratio;
-    // LRUCache *db_small_page;
-    // integer *addr_map;
-    // int *vis_count;
 } db_myfork_lru_infomation;
 
 
